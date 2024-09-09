ff65e62a62aa2a848b77f1aee71dff5a0e7b50eb
赵开泰 <3012579700@qq.com>
2024-09-10 00:10:15 +0800

    制作了背景

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
   <div class="common-layout">
+    <div class="layer1"></div>
+    <div class="layer2"></div>
+    <div class="layer3"></div>
     <el-container>
       <el-header>
         <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" :ellipsis="false"
           @select="handleSelect">
-          <el-tooltip content="首页" placement="bottom" effect="light">
-            <el-menu-item index="home">
-              <img style="width: 100px" src="./images/element-plus-logo.svg" alt="logo" />
-            </el-menu-item>
-          </el-tooltip>
+          <el-menu-item index="home">
+            <img style="width: 100px" src="./images/element-plus-logo.svg" alt="logo" />
+          </el-menu-item>
           <el-menu-item index="playground">Playground</el-menu-item>
           <el-menu-item index="buy">购买</el-menu-item>
           <el-menu-item index="console">控制台</el-menu-item>
-          <el-tooltip content="我的" placement="bottom" effect="light">
-            <el-menu-item index="my">
-              <div class="avatar">
-                <el-avatar src="https://c-ssl.dtstatic.com/uploads/blog/202304/15/20230415081411_f2e46.thumb.400_0.jpg">
-                  {{ avatarText }}
-                </el-avatar>
-              </div>
-            </el-menu-item>
-          </el-tooltip>
+          <el-menu-item index="my">
+            <div class="avatar">
+              <el-avatar src="https://c-ssl.dtstatic.com/uploads/blog/202304/15/20230415081411_f2e46.thumb.400_0.jpg">
+                {{ avatarText }}
+              </el-avatar>
+            </div>
+          </el-menu-item>
         </el-menu>
       </el-header>
       <el-main>
@@ -39,17 +38,72 @@ const avatarText = ref('登录')
 
 
 const handleSelect = (key: string) => {
-  router.push({ name: key });
+  if (key === 'console') {
+    console.log('跳转控制台');
+  } else {
+    router.push({ name: key });
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-:root {
-  --el-color-primary: #000;
-}
-
 .common-layout {
-  background: #fff;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  overflow-y: auto;
+  background: radial-gradient(ellipse at bottom, #1b2735 0%, #090a0f 100%);
+
+  @function getShadows($n) {
+    $shadows: '#{random(100)}vw #{random(100)}vh #fff';
+
+    @for $i from 2 through $n {
+      $shadows: '#{$shadows}, #{random(100)}vw #{random(100)}vh #fff';
+    }
+
+    @return unquote($shadows);
+  }
+
+  $duration: 400s;
+  $count: 1000;
+
+  @for $i from 1 through 3 {
+    $duration: floor($duration / 2);
+    $count: floor($count / 2);
+
+    .layer#{$i} {
+      $size: #{$i}px;
+
+      position: fixed;
+      width: $size;
+      height: $size;
+      border-radius: 50%;
+      background: red;
+      left: 0;
+      top: 0;
+      box-shadow: getShadows($count);
+      animation: moveUp $duration linear infinite;
+
+      &::after {
+        content: '';
+        position: fixed;
+        left: 0;
+        top: 100vh;
+        width: $size;
+        height: $size;
+        border-radius: inherit;
+        box-shadow: inherit;
+      }
+    }
+  }
+
+  @keyframes moveUp {
+    100% {
+      transform: translateY(-100vh);
+    }
+  }
 }
 
 .el-header {
@@ -64,7 +118,11 @@ const handleSelect = (key: string) => {
     }
 
     .el-menu-item {
-      color: gray;
+      color: #fff;
+      background: linear-gradient(white, #38495a);
+      background-clip: text;
+      -webkit-background-clip: text;
+      color: transparent;
 
       .avatar {
         display: flex;
@@ -81,11 +139,13 @@ const handleSelect = (key: string) => {
 
       &.is-active {
         background: none;
+        color: #fff !important;
       }
 
       &:not(.is-disabled):focus,
       &:not(.is-disabled):hover {
         background: none;
+        color: #fff !important;
       }
 
       @for $i from 1 through 5 {
@@ -108,7 +168,7 @@ const handleSelect = (key: string) => {
 }
 
 .el-main {
-  --el-main-padding: 30px 10vw;
+  --el-main-padding: 0;
   display: flex;
   justify-content: center;
 }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,37 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+  <div class="layout">
+    <div class="title">
+      Crawl&LLM
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 
 </script>
+
+<style lang="scss" scoped>
+.layout {
+  height: 100vh;
+  overflow: hidden;
+
+  .title {
+    position: absolute;
+    top: 50%;
+    left: 0;
+    right: 0;
+    color: #fff;
+    text-align: center;
+    font-family: 'lato', sans-serif;
+    font-weight: 300;
+    font-size: 50px;
+    letter-spacing: 10px;
+    margin-top: -60px;
+    margin-left: 10px;
+    background: linear-gradient(white, #38495a);
+    background-clip: text;
+    -webkit-background-clip: text;
+    color: transparent;
+  }
+}
+</style>
